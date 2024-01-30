795728b7ad3cb8c513167ac227df577f45f26ddb
Rodrigo Maciel <rodrigopedro.m@gmail.com>
2024-01-30 16:35:04 -0300

    criando o componente taskLits
    criando o componente taskLits

--- src/components/tarefa-component/task-list-component/task-list.tsx
+++ src/components/tarefa-component/task-list-component/task-list.tsx
@@ -1,10 +1,24 @@
 import taskListStyle from './task-list.module.css';
 
 
-export const TaskList = () => {
+type  TaskItemProps = {
+    taskImg?:  string | null,
+    taskUsuName:  string,
+    taskTitle:    string,
+    taskSubTitle?:  string | number,
+    taskTime?:      Date | string | number | null,
+    taskDateCriacao?:    Date | string | undefined,
+    tskDateCoclusao?:      Date | string | undefined,
+    tskFunction?  : () => void,
+    taskColor?:  "taskColor1" | "taskColor2" |"taskColor3" |"taskColor4" |"taskColor5" |"taskColor6" |"taskColor7" |"taskColor8" |"taskColor9" |"taskColor10" | "taskColor11",
+}
+
+export const TaskList = (task: TaskItemProps) => {
+    const taskColorClass = task.taskColor ? taskListStyle[task.taskColor] : '';
     return(
-        <div className={taskListStyle.taskLitContainer}>
-            1
+        <div className={`${taskListStyle.taskLitContainer} ${taskColorClass}`}>
+            <div className={taskListStyle.childrenContainerUm}>1</div>
+            <div className={taskListStyle.childrenContainerDois}>2</div>
         </div>
     );
 }
