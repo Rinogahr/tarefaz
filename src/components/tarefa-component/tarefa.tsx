5a9786853fa4a72b71b0bea76fe19ab2a7ef9e94
Rodrigo Maciel <rodrigopedro.m@gmail.com>
2024-02-27 11:51:31 -0300

    Update tarefa.tsx

--- src/components/tarefa-component/tarefa.tsx
+++ src/components/tarefa-component/tarefa.tsx
@@ -9,14 +9,13 @@ export const Tarefa = () =>{
     const params = useParams();
     const taskList = tarefasDiarias.tarefasDiarias;
     const [tarefaFiltrada, setTarefaFiltrada] = useState<{ usuario: { id: number; name: string; dados: string; photpth: string; }[]; titulo: string; descricao: string; dataInicio: string; dataFim: string; feito: boolean; }[]>([]);
-    const [cont, setCont] = useState(0)
+    let color = "taskColor5";
     // const [tarefaFiltrada, setTarefaFiltrada] = useState<{ usuario: any}[]>([]);
 
     // Filtrar as tarefas e, em seguida, atualizar o estado
     useEffect(() => {
         const tarefasFiltradas = taskList.filter((item) => item.usuario[0].id == Number(params.id));
         setTarefaFiltrada(tarefasFiltradas);
-        setCont(+1);
     }, [params.id, taskList]); // Dependências do useEffect
 
     // Use tarefaFiltrada conforme necessário em seu componente
@@ -29,7 +28,7 @@ export const Tarefa = () =>{
              "Tarefa não encontrada" :  
              tarefaFiltrada.map( (task) => {
                 return <TaskList key={task.titulo}
-                taskColor="taskColor1"
+                taskColor={color}
                 taskTitle={task.titulo}
                 taskSubTitle={task.descricao}
                 taskUsuName={task.usuario[0].name}
